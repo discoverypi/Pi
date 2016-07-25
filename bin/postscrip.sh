596056b438e87cb56a0201cba9eb27a7f5276a98
jr <jr@MacPro.(none)>
2016-07-24 23:41:11 -0500

    update script

--- bin/postscrip.sh
+++ bin/postscrip.sh
@@ -1,6 +1,9 @@
-find usr bin sbin lib -type f -exec /toolsx/bin/arm-unknown-linux-gnueabi-strip --strip-all {} ';'
 mv /Pi/clfs/toolsx /Pi/toolsx
 mv /Pi/clfs/tools /Pi/tools
+cd /Pi/clfs
+find /Pi/clfs -user pi -exec chown root:root {} \;
+find /Pi/clfs -type d -user pi -exec chown root:root {} \;
+find /Pi/clfs -type l -user pi -exec -h chown root:root {} \;
 ln -s usr /Pi/clfs/toolsx
 ln -s ../../bin/bash /Pi/clfs/usr/bin
 cp /Pi/src/ld.so.conf /Pi/clfs/etc/ld.so.conf
@@ -12,3 +15,18 @@ cd /Pi/clfs/usr/share/zoneinfo
 tar -xf /Pi/src/zoneinfo.tar
 cd /Pi/clfs 
 touch .firstboot
+grep -Irl toolsx /Pi/clfs/usr/* > /tmp/t.log
+fixtoolsx.sh /tmp/t.log
+cp -r /Pi/firmware/boot /Pi/clfs/boota
+cp -r /Pi/clfs/boot/* /Pi/clfs/boota/.
+mv /Pi/clfs/boot /Pi/boot.org
+mv /Pi/clfs/boota  /Pi/clfs/boot
+cd /Pi/clfs/lib/modules/
+ln -s 4.4.8-v7 4.4.8-v7+
+mkdir /Pi/clfs/usr/share/pios
+cp /Pi/postpkg.tar /Pi/clfs/usr/share/pios/.
+cd /Pi/clfs/etc/pam.d
+tar -xf /Pi/src/pam.d.tar
+cd /Pi/clfs/etc/rc.d
+tar -xf /Pi/src/rc.d.tar
+
